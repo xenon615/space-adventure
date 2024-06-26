7e7c345fa6cd610421492a7121ce6f01903e5cb9
xenon615 <xenon615@limeira.eu>
2024-06-26 15:38:52 +0300

    event-driven registration and update widgets,

--- src/ui.rs
+++ src/ui.rs
@@ -1,42 +1,81 @@
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
-use crate::camera::CamViewState;
-use crate::{Target, drone::{Drone, Supplies},camera::{Focus, Cam}};
+
 pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn);
-        app.add_systems(Update, (
-            update_indicators, 
-            read_messages,
-            move_crosshair.run_if(not(in_state(CamViewState::Free)))
-        ));
-
-        app.add_systems(OnEnter(CamViewState::Free), show_crosshair);
-        app.add_systems(OnExit(CamViewState::Free), show_crosshair);
-
+        app.add_event::<RegisterWidgets>();
+        app.add_event::<UpdateWidgets>();
+        app.add_systems(Update, register_widgets.run_if(on_event::<RegisterWidgets>()));
+        app.add_systems(Update, update_widget.run_if(on_event::<UpdateWidgets>()));
     }
 }
 
-#[derive(Component, PartialEq, Debug)]
-pub enum  UIIndicator {
-    Velocity,
-    Distance,
-    Direction,
-    Elevation,
-    Fluel,
-    Message,
+// ---
+
+#[derive(Debug)]
+pub struct WidgetRegData{
+    pub parent: ULayout,
+    pub wtype: WType,
+    pub start: i16,
+    pub span: u16,
+    pub key: &'static str,
+    pub label: &'static str,
+    pub image: Option<Handle<Image>>
 }
 
-#[derive(Component)]
-pub struct Crosshair;
+#[derive(Event, Debug)]
+pub struct RegisterWidgets(pub Vec<WidgetRegData>); 
+
+#[derive(Debug)]
+pub struct WidgetUpdateData {
+    key: &'static str,
+    color: Option<Color>,
+    value: f32
+}
+
+impl WidgetUpdateData {
+    pub fn from_key_value(key: &'static str, value: f32) -> Self {
+        Self {key, value, color: None}
+    }
+
+    pub fn from_key_value_color(key: &'static str, value: f32, color: Color) -> Self {
+        Self {key, value, color: Some(color)}
+    }
+}
+
+#[derive(Event, Debug)]
+pub struct UpdateWidgets(pub Vec<WidgetUpdateData>);
+
+#[derive(Component, PartialEq)]
+pub struct WKey(pub &'static str);
+
+// + LAYOUT====================
+
+#[derive(Component, PartialEq, Debug)]
+pub enum ULayout {
+    Wrapper,
+    Header,
+    Footer,
+    Body,
+    SidebarLeft,
+    Content,
+    SidebarRight
+}
+
+#[derive(Component, PartialEq, Debug)]
+pub enum WType {
+    Text,
+    Image
+}
+
+// - LAYOUT====================
 
 fn spawn(
     mut commands: Commands,
-    asset: ResMut<AssetServer>
 ) {
 
-    commands.spawn(
+    commands.spawn((
         NodeBundle {
             style: Style {
                 width: Val::Vw(100.),
@@ -47,258 +86,187 @@ fn spawn(
             },
             ..default() 
         },
-    )
+        ULayout::Wrapper
+    ))
     .with_children(|p| {
-        p.spawn(
-            NodeBundle {
-                style: Style {
-                    height: Val::Px(100.),
-                    width: Val::Percent(100.),
-                    flex_direction: FlexDirection::Row,
-                    justify_content: JustifyContent::SpaceEvenly,
-                    ..default()
-                },
-                ..default()
-            }
-        )
-
-        .with_children(|p| {
-                p.spawn((
-                    TextBundle::from_section(
-                        "", 
-                        TextStyle {
-                            color: Color::WHITE,
-                            font_size: 30.,
-                            ..default()
-                        }
-                    ),
-                    UIIndicator::Velocity
-                ));
-            
-                p.spawn((
-                    TextBundle::from_section(
-                        "", 
-                        TextStyle {
-                            color: Color::GREEN,
-                            font_size: 30.,
-                            ..default()
-                        }
-                    ),
-                    UIIndicator::Distance
-                ));
-
-                p.spawn((
-                    TextBundle::from_section(
-                        "", 
-                        TextStyle {
-                            color: Color::WHITE,
-                            font_size: 30.,
-                            ..default()
-                        }
-                    ),
-                    UIIndicator::Elevation
-                ));
-
-                p.spawn((
-                    TextBundle::from_section(
-                        "", 
-                        TextStyle {
-                            color: Color::RED,
-                            font_size: 30.,
-                            ..default()
-                        }
-                    ),
-                    UIIndicator::Fluel
-                ));
-
-                p.spawn(
-                    NodeBundle {
-                        style : Style {
-                            width: Val::Px(50.),
-                            height: Val::Px(50.),
-                            ..default()
-                        },
-                        ..default()
-                    }
-                )
-                .with_children(|n| {
-                    n.spawn((
-                        ImageBundle {
-                            image: UiImage::new(asset.load("images/arrow.png")),
-                            ..default()
-                        },
-                        UIIndicator::Direction
-                    ));
-                }) 
-                ;
-            })
-            ;
-        p.spawn(
-            NodeBundle {
-                style: Style {
-                    flex_grow: 1.,
-                    width: Val::Percent(100.),
-                    ..default()
-                },
-                ..default()
-            }
-        );
-
-        p.spawn(
-            NodeBundle {
-                style: Style {
-                    justify_content: JustifyContent::SpaceEvenly,
-                    ..default()
-                },
-                ..default()
-            }
-        )
-        .with_children(|p| {
-            p.spawn((
-                TextBundle::from_section(
-                    "", 
-                    TextStyle {
-                        color: Color::GOLD,
-                        font_size: 30.,
-                        ..default()
-                    }
-                ),
-                UIIndicator::Message
-            ));
-        })
-        ;
+        p.spawn(( get_bar(), ULayout::Header));
 
         p.spawn((
-            ImageBundle {
-                style: Style {
-                    width: Val::Px(100.),
-                    height: Val::Px(100.),
-                    position_type: PositionType::Absolute,
-                    left: Val::Px(0.),
-                    top: Val::Px(0.),
-                    ..default()
-                },
-                image: UiImage::new(asset.load("images/crosshair.png")),
-                ..default()
-            },
-            Crosshair
-        ));
-    })    
-    
-    ;
+            get_body(),
+            ULayout::Body
+        ))
+        .with_children(|b| {
+            b.spawn((get_sidebar(), ULayout::SidebarLeft));
+            b.spawn((get_content(), ULayout::Content));
+            b.spawn((get_sidebar(), ULayout::SidebarRight));
+        });
+
+        p.spawn((get_bar(), ULayout::Footer));
+    });
 
 } 
 
-fn update_indicators(
-    drone_q: Query<(&Velocity, &Transform, &Supplies), (With<Drone>, With<Focus>,  Without<Target>)>,
-    mut i_q: Query<(&mut Text, &UIIndicator)>,
-    mut a_q: Query<(&mut Transform, &UIIndicator), (Without<Drone>, Without<Target>, Without<Focus>)>,
-    target_q: Query<&Transform, (With<Target>, Without<Drone>, Without<Focus>)>,
-) {
-    let target_translation = if let Ok(target_transform) =  target_q.get_single()  {
-        target_transform.translation
-    } else {
-        Vec3::ZERO
-    };
+// ==================================================================
 
-    let Ok((v, drone_transform, supplies)) = drone_q.get_single() else {
-        return;
-    };
-
-
-    let to_target = target_translation - drone_transform.translation;
-
-    for  (mut text, itype) in i_q.iter_mut() {
-        match itype {
-            UIIndicator::Velocity => {
-                text.sections[0].value = format!("Vel: {:.2}",v.linvel.length())
-            },
-            UIIndicator::Distance => {
-                text.sections[0].value = format!("Dist XZ: {:.2}",to_target.reject_from(Vec3::Y).length())
-            },
-
-            UIIndicator::Elevation => {
-                text.sections[0].value = format!("Dist Y: {:.2}", drone_transform.translation.y -  target_translation.y)
-            },
-
-            UIIndicator::Fluel=> {
-                text.sections[0].value = format!("Fluel: {:.2}", supplies.fluel_get());
-                text.sections[0].style.color = if supplies.fluel_limit() {Color::ORANGE_RED} else {Color::YELLOW_GREEN};
-            },
-            _ => ()
-        }
-    }
-
-    for  (mut trans, itype) in a_q.iter_mut() { 
-        if *itype == UIIndicator::Direction {
-            let to_target_xz = to_target.normalize().reject_from_normalized(Vec3::Y);
-            let forward_xz: Vec3 = drone_transform.forward().into();
-            let dot = to_target_xz.dot(forward_xz);
-            let sign = to_target_xz.cross(forward_xz).y.signum();
-            let angle = dot.acos() * sign;
-            trans.rotation = Quat::from_rotation_z(angle) ;    
-        }
-        
-    }
-}
-
-use crate::GameMessage;
-
-// ---
-
-fn read_messages(
-    mut i_q: Query<(&mut Text, &UIIndicator)>,
-    mut ev_reader: EventReader<GameMessage>
-) {
-    for e in ev_reader.read() {
-        for (mut text, _itype) in i_q.iter_mut().filter(|(_, itype0)| {
-            **itype0 == UIIndicator::Message
-        }) {
-            text.sections[0].value = e.0.clone();
-        }
+fn get_bar() -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            height:Val::Px(50.),
+            width: Val::Percent(100.),
+            display: Display::Grid,
+            grid_template_columns: RepeatedGridTrack::percent(10, 10.),
+            grid_template_rows: GridTrack::percent(100.),
+            ..default()
+        },
+        background_color: Color::rgba(0., 1., 0., 0.005).into(),
+        ..default()
     }
 }
 
 // ---
 
-fn move_crosshair(
-    focus_q: Query<&Transform, (With<Focus>, Without<Target>)>,   
-    target_q: Query<&Transform, (With<Target>, Without<Focus>)>,
-    mut crosshair_q: Query<&mut Style, With<Crosshair>>,
-    camera_query: Query<(&Camera, &GlobalTransform), With<Cam>>,
+fn get_body () -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            flex_grow: 2.0,
+            width: Val::Percent(100.),
+            flex_direction: FlexDirection::Row,
+            ..default()
+        },
+        ..default()
+    }
+}
+
+// ---
+
+fn get_sidebar () -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            width: Val::Px(100.),
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        background_color: Color::rgba(0., 0., 1., 0.005).into(),
+        ..default()
+    }
+}
+
+// ---
+
+fn get_content () -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            flex_grow: 2.0,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        ..default()
+    }
+}
+
+// ---
+
+fn register_widgets(
+    mut commands: Commands,
+    mut evs: EventReader<RegisterWidgets>,
+    nodes_q: Query<(Entity, &ULayout)>,
 ) {
-    for (camera, camera_transform) in camera_query.iter() {
-        for focus_trans  in focus_q.iter() {
-            for mut style in crosshair_q.iter_mut() {
-                let mut dist = 100.;
-                if let Ok(target) = target_q.get_single() {
-                    dist = target.translation.distance(focus_trans.translation);
-                }
+    for ev in evs.read() {
+        for w in ev.0.iter() { 
+            for (node_ent, l)  in nodes_q.iter() {
+                if *l == w.parent {
+
+                    let widget_ent = commands.spawn(
+                        NodeBundle {
+                            style: Style {
+                                padding: UiRect::all(Val::Px(5.)),
+                                display: Display::Flex,
+                                flex_direction: FlexDirection::Row,
+                                grid_column: GridPlacement::start_span(w.start, w.span),
+                                justify_content: JustifyContent::Center,
+                                align_items:AlignItems::Center, 
+                                ..default()
+                            },
+                            ..default()
+                        }
+                    )
+                    .with_children(| p | {
+                        if w.wtype == WType::Text {
+                            p.spawn(
+                                TextBundle::from_section(
+                                    format!("{}: ", w.label), 
+                                    TextStyle {
+                                        color: Color::GOLD.into(),
+                                        font_size: 25.,
+                                        ..default()
+                                    } 
+                                ).with_style(Style {
+                                    flex_basis: Val::Percent(50.),
+                                    ..default()
+                                })
+                            );
+                            p.spawn((
+                                TextBundle::from_section(
+                                    "", 
+                                    TextStyle {
+                                        color: Color::GOLD.into(),
+                                        font_size: 25.,
+                                        ..default()
+                                    } 
+                                ).with_style(Style {
+                                    flex_basis: Val::Percent(50.),
+                                    ..default()
+                                }),
+                                WKey(w.key),
+                                WType::Text
+                            ));
     
-                let far = focus_trans.translation + focus_trans.forward() * dist;
-                match camera.world_to_viewport(camera_transform, far)
-                {
-                    Some(coords) => {
-                        style.left = Val::Px(coords.x - 50.);
-                        style.top = Val::Px(coords.y - 50.);
-                    },
-                    None => ()
+                        } else {
+                            p.spawn((
+                                ImageBundle {
+                                    style: Style {
+                                        width: Val::Px(50.),
+                                        height: Val::Px(50.),
+                                        ..default()
+                                    },
+                                    image: UiImage::new(w.image.clone().unwrap()),
+                                    ..default()
+                                },
+                                WKey(w.key),
+                                WType::Image
+                            ));
+                        }
+                    })
+                    .id();
+                    commands.entity(node_ent).add_child(widget_ent);
+                } 
+            }
+        }
+    }
+    
+}
+
+// ---
+
+fn update_widget(
+    mut reader : EventReader<UpdateWidgets>,
+    mut widget_q: Query<(Option< &mut Text>, &mut Transform, &WKey)> 
+) {
+    for ev in reader.read() {
+        for w in ev.0.iter() {
+            for (text_o, mut trans ,key) in widget_q.iter_mut() {
+                if w.key == key.0 {
+                    if let Some(mut text) = text_o {
+                        text.sections[0].value = format!("{:.2}", w.value);
+                        if let Some(c) = w.color {
+                            text.sections[0].style.color = c;                        
+                        }
+                    } else {
+                        trans.rotation = Quat::from_rotation_z(w.value) ;    
+                    }
                 }
             }
         }
     }
 }
 
-// ---
-
-fn show_crosshair(
-    mut crosshair_q: Query<&mut Visibility, With<Crosshair>>,
-) {
-    if let Ok(mut v) = crosshair_q.get_single_mut() {
-        *v = match *v {
-            Visibility::Hidden => Visibility::Inherited,
-            Visibility::Inherited => Visibility::Hidden,
-            Visibility::Visible => Visibility::Visible
-        }
-    }
-}
